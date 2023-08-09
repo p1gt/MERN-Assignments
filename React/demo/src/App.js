f469d455e34de58c68f5b5d59cbe1d3e39e9dbdc
p1gt <ranaranap59@gmail.com>
2023-08-09 22:37:27 +0300

    first jsx

---
+++ React/demo/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<h1>hello dojo!</h1>
+			<h2>things i need to do:</h2>
+			<ul>
+				<li>learn react</li>
+				<li>climb mt. everest</li>
+				<li>run a marathon</li>
+				<li>feed the dogs</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
