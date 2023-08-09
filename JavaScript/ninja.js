3a4005d1e7098f688ff40883eca1d16577e19350
p1gt <ranaranap59@gmail.com>
2023-08-09 15:39:55 +0300

    sensei

--- JavaScript/ninja.js
+++ JavaScript/ninja.js
@@ -26,3 +26,19 @@ class Ninja {
 goku = new Ninja("Goku", Infinity, Infinity, Infinity)
 
 goku.sayName().showStats().drinkSake().showStats();
+
+class Sensei extends Ninja {
+    constructor(name, strength = 10, speed = 10, health = 200, wisdom = 10) {
+        super(name, strength, speed, health);
+        this.wisdom = wisdom;
+    }
+
+    speakWisdom() {
+        super.drinkSake();
+        console.log("Anger is the ultimate destroyer of your own peace of mind.");
+    }
+}
+
+roshi = new Sensei("Roshi", 0, 0, 0, 0);
+
+roshi.sayName().showStats().speakWisdom().showStats();
