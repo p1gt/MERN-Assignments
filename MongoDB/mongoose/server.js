07b210e5f8081325f6ab01c7f6003fccee861abb
p1gt <ranaranap59@gmail.com>
2023-08-23 05:26:27 +0300

    mongoose

--- MongoDB/mongoose/server.js
+++ MongoDB/mongoose/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-require("./config/mongoose.config");
+require("./server/config/mongoose.config");
 const port = process.env.PORT;
 app.use(express.json(), express.urlencoded({ extended: true }));
 
-const AllMyUserRoutes = require("./routes/user.routes");
+const AllMyUserRoutes = require("./server/routes/user.routes");
 AllMyUserRoutes(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
